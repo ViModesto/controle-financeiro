7b4740ccfb2f8af45df4618e3cceafdc27aa2fc6
ViModesto <vitoriamodestoo2005@gmail.com>
2025-05-22 19:27:08 -0300

    feat: criação da nova tela de login

--- src/Pages/login/index.js
+++ src/Pages/login/index.js
@@ -1,39 +1,83 @@
-import React from 'react';
-import { StyledWrapper } from './style';
+import React, { useState } from "react";
+import { StyledWrapper } from "./style";
 
-const Form = ({onLogin}) => {
+const Form = ({ onLogin }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!username.trim() || !password.trim()) {
+      setError("Preencha todos os campos.");
+      return;
+    }
+
+    setError("");
+    onLogin({ username, password });
+  };
+
   return (
     <StyledWrapper>
-      <form className="form">
-        <div id="login-area">
-          <p>LOGIN</p>
-        </div>
-        <div id="email-area">
-          <input placeholder="EMAIL" id="email" className="input" type="text" />
-        </div>
-        <div id="password-area">
-          <input placeholder="PASSWORD" id="password" className="input" type="password" />
-          <a href="/forgot-password">Forgot password?</a>
-        </div>
-        <div id="footer-area">
-          <button onClick={onLogin}>Log In</button>
-          <div id="text-inside">
-            <p>Don't have an account?</p>
-            {/* <a id="aaaa">Sign Up</a> */}
-          </div>
-        </div>
-        <div id="background-color" />
-        <div id="whitefilter" />
-        <div id="link-circle">
-          <svg xmlns="http://www.w3.org/2000/svg" width={35} height={35} viewBox="0 0 24 24" fill="#6041bf">
-            <path d="M12 0c6.627 0 12 5.373 12 12s-5.373 12-12 12S0 18.627 0 12 5.373 0 12 0zm4 7.278V4.5h-2.286c-2.1 0-3.428 1.6-3.428 3.889v1.667H8v2.777h2.286V19.5h2.857v-6.667h2.286L16 10.056h-2.857V8.944c0-1.11.572-1.666 1.714-1.666H16z" />
+      <form className="form_main" onSubmit={handleSubmit}>
+        <p className="heading">Login</p>
+
+        <div className="inputContainer">
+          <svg
+            viewBox="0 0 16 16"
+            fill="#2e2e2e"
+            height={16}
+            width={16}
+            xmlns="http://www.w3.org/2000/svg"
+            className="inputIcon"
+          >
+            <path d="M13.106 7.222c0-2.967-2.249-5.032-5.482-5.032-3.35 0-5.646 2.318-5.646 5.702 0 3.493 2.235 5.708 5.762 5.708.862 0 1.689-.123 2.304-.335v-.862c-.43.199-1.354.328-2.29.328-2.926 0-4.813-1.88-4.813-4.798 0-2.844 1.921-4.881 4.594-4.881 2.735 0 4.608 1.688 4.608 4.156 0 1.682-.554 2.769-1.416 2.769-.492 0-.772-.28-.772-.76V5.206H8.923v.834h-.11c-.266-.595-.881-.964-1.6-.964-1.4 0-2.378 1.162-2.378 2.823 0 1.737.957 2.906 2.379 2.906.8 0 1.415-.39 1.709-1.087h.11c.081.67.703 1.148 1.503 1.148 1.572 0 2.57-1.415 2.57-3.643zm-7.177.704c0-1.197.54-1.907 1.456-1.907.93 0 1.524.738 1.524 1.907S8.308 9.84 7.371 9.84c-.895 0-1.442-.725-1.442-1.914z" />
           </svg>
-          <svg xmlns="http://www.w3.org/2000/svg" width={35} height={35} viewBox="0 0 24 24" fill="#6041bf">
-            <path d="M12 0c6.6274 0 12 5.3726 12 12s-5.3726 12-12 12S0 18.6274 0 12 5.3726 0 12 0zm3.115 4.5h-6.23c-2.5536 0-4.281 1.6524-4.3805 4.1552L4.5 8.8851v6.1996c0 1.3004.4234 2.4193 1.2702 3.2359.7582.73 1.751 1.1212 2.8818 1.1734l.2633.006h6.1694c1.3004 0 2.389-.4234 3.1754-1.1794.762-.734 1.1817-1.7576 1.2343-2.948l.0056-.2577V8.8851c0-1.2702-.4234-2.3589-1.2097-3.1452-.7338-.762-1.7575-1.1817-2.9234-1.2343l-.252-.0056zM8.9152 5.8911h6.2299c.9072 0 1.6633.2722 2.2076.8166.4713.499.7647 1.1758.8103 1.9607l.0063.2167v6.2298c0 .9375-.3327 1.6936-.877 2.2077-.499.4713-1.176.7392-1.984.7806l-.2237.0057H8.9153c-.9072 0-1.6633-.2722-2.2076-.7863-.499-.499-.7693-1.1759-.8109-2.0073l-.0057-.2306V8.885c0-.9073.2722-1.6633.8166-2.2077.4712-.4713 1.1712-.7392 1.9834-.7806l.2242-.0057h6.2299-6.2299zM12 8.0988c-2.117 0-3.871 1.7238-3.871 3.871A3.8591 3.8591 0 0 0 12 15.8408c2.1472 0 3.871-1.7541 3.871-3.871 0-2.117-1.754-3.871-3.871-3.871zm0 1.3911c1.3609 0 2.4798 1.119 2.4798 2.4799 0 1.3608-1.119 2.4798-2.4798 2.4798-1.3609 0-2.4798-1.119-2.4798-2.4798 0-1.361 1.119-2.4799 2.4798-2.4799zm4.0222-2.3589a.877.877 0 1 0 0 1.754.877.877 0 0 0 0-1.754z" />
+          <input
+            placeholder="Username"
+            id="username"
+            className="inputField"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+
+        <div className="inputContainer">
+          <svg
+            viewBox="0 0 16 16"
+            fill="#2e2e2e"
+            height={16}
+            width={16}
+            xmlns="http://www.w3.org/2000/svg"
+            className="inputIcon"
+          >
+            <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z" />
           </svg>
-          <svg xmlns="http://www.w3.org/2000/svg" width={35} height={35} viewBox="0 0 24 24" fill="#6041bf">
-            <path d="M12 0c6.627 0 12 5.373 12 12s-5.373 12-12 12S0 18.627 0 12 5.373 0 12 0zM8.951 9.404H6.165V17.5H8.95V9.404zm6.841-.192c-1.324 0-1.993.629-2.385 1.156l-.127.181V9.403h-2.786l.01.484c.006.636.007 1.748.005 2.93l-.015 4.683h2.786v-4.522c0-.242.018-.484.092-.657.202-.483.66-.984 1.43-.984.955 0 1.367.666 1.408 1.662l.003.168V17.5H19v-4.643c0-2.487-1.375-3.645-3.208-3.645zM7.576 5.5C6.623 5.5 6 6.105 6 6.899c0 .73.536 1.325 1.378 1.392l.18.006c.971 0 1.577-.621 1.577-1.398C9.116 6.105 8.53 5.5 7.576 5.5z" />
-          </svg>
+          <input
+            placeholder="Password"
+            id="password"
+            className="inputField"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        {error && (
+          <p style={{ color: "red", fontSize: "0.9rem", marginTop: "0.5rem" }}>
+            {error}
+          </p>
+        )}
+
+        <button id="button" type="submit">
+          Submit
+        </button>
+
+        <div className="signupContainer">
+          <p>Don't have any account?</p>
+          {/* <a href="">Sign up</a> */}
         </div>
       </form>
     </StyledWrapper>
